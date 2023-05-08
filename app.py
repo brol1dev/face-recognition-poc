882b4a9ccc13d1081719bccec01c00596da3c422
Eric Vargas <eric.vargas.mx@gmail.com>
2023-05-07 23:23:38 -0500

    Disable deepface

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def compare_images():
         photo_data = base64.b64encode(photo.read()).decode()
         photo_data = 'data:image/' + photo_extension + ';base64,' + photo_data
         print('DeepFace verify starting...')
-        result = DeepFace.verify(id_photo_data, photo_data)
-        pprint(result)
-        return render_template('compare-images.html', is_same_face=result['verified'])
+        # result = DeepFace.verify(id_photo_data, photo_data)
+        # pprint(result)
+        # return render_template('compare-images.html', is_same_face=result['verified'])
     
     return render_template('compare-images.html')
 
