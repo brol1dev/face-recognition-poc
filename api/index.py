5995bceaba809beec27af29daf2f66840e658134
Eric Vargas <eric.vargas.mx@gmail.com>
2023-05-07 19:01:31 -0500

    Minimal template index.html

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from pathlib import Path
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/compare-images', methods=['POST'])
+def compare_images():
+    return 'Compare Images'
+
+if __name__ == '__main__':
+    app.run(debug=True)
