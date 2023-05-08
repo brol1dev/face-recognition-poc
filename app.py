7c9547e5faa75a3003e93d14295d0a01438ede6d
Eric Vargas <eric.vargas.mx@gmail.com>
2023-05-07 23:34:39 -0500

    Enable DeepFace

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from pathlib import Path
 import os
 from deepface import DeepFace
-from werkzeug.utils import secure_filename
 from pprint import pprint
 import base64
 
@@ -30,9 +29,9 @@ def compare_images():
         photo_data = base64.b64encode(photo.read()).decode()
         photo_data = 'data:image/' + photo_extension + ';base64,' + photo_data
         print('DeepFace verify starting...')
-        # result = DeepFace.verify(id_photo_data, photo_data)
-        # pprint(result)
-        # return render_template('compare-images.html', is_same_face=result['verified'])
+        result = DeepFace.verify(id_photo_data, photo_data)
+        pprint(result)
+        return render_template('compare-images.html', is_same_face=result['verified'])
     
     return render_template('compare-images.html')
 
