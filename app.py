87218b0c23bec91f05517f222146b5a80127c574
Eric Vargas <eric.vargas.mx@gmail.com>
2023-05-07 20:44:23 -0500

    Use deepface verify

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from pathlib import Path
+import os
+from deepface import DeepFace
+from werkzeug.utils import secure_filename
+from pprint import pprint
 
 app = Flask(__name__)
+
+app.config['IMAGE_UPLOADS'] =  str(Path(__file__).parent / 'uploads')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/compare-images', methods=['POST'])
+@app.route('/compare-images', methods=['GET', 'POST'])
 def compare_images():
-    return 'Compare Images'
+    if request.files:
+        id_photo = request.files['id-photo']
+        photo = request.files['photo']
+        id_photo_filename = secure_filename(id_photo.filename)
+        photo_filename = secure_filename(photo.filename)
+        id_photo_path = os.path.join(app.config['IMAGE_UPLOADS'], id_photo_filename)
+        photo_path = os.path.join(app.config['IMAGE_UPLOADS'], photo_filename)
+        id_photo.save(id_photo_path)
+        photo.save(photo_path)
+        result = DeepFace.verify(id_photo_path, photo_path)
+        pprint(result)
+        return render_template('compare-images.html', is_same_face=result['verified'])
+    
+    return render_template('compare-images.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
